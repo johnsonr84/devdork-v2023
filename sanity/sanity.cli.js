3dfd8976b8a71b1eb706c4cffe2958641b9d28ab
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-07 00:33:38 -0700

    install and set up sanity

---
+++ sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cnx08xq5',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.js
@@ -0,0 +1,11 @@
+import pageInfo from './pageInfo'
+import experience from './experience'
+import project from './project'
+import skill from './skill'
+import social from './social'
+
+export const schemaTypes = [ pageInfo,
+    experience,
+    project,
+    skill,
+    social, ]
