b6e26e049220689a344334e2f3def57c596f6549
Dev Dork <robertcjohnson1984@gmail.com>
2023-02-07 20:20:25 -0700

    create sanity.ts add to .gitignore

--- sanity/schemas/experience.js
+++ sanity/schemas/experience.js
@@ -1,53 +1,52 @@
-import {defineField, defineType} from 'sanity'
-
-export default defineType({
-  name: 'experience',
-  title: 'Experience',
-  type: 'document',
+export default {
+  name: "experience",
+  title: "Experience",
+  type: "document",
   fields: [
-    defineField({
-      name: 'jobTitle',
-      title: 'JobTitle',
-      type: 'string',
-    }),
-    defineField({
-      name: 'companyImage',
-      title: 'Company Image',
-      type: 'image',
+    {
+      name: "jobTitle",
+      title: "JobTitle",
+      type: "string",
+    },
+    {
+      name: "companyImage",
+      title: "Company Image",
+      type: "image",
       options: {
         hotspot: true,
       },
-    }),
-    defineField({
-      name: 'company',
-      title: 'Company',
-      type: 'text',
-    }),
-    defineField({
-      name: 'dateStarted',
-      title: 'DateStarted',
-      type: 'date',
-    }),
-    defineField({
-      name: 'dateEnded',
-      title: 'DateEnded',
-      type: 'date',
-    }),
-    defineField({
-      name: 'isCurrentlyWorkingHere',
-      title: 'IsCurrentlyWorkingHere',
-      type: 'boolean',
-    }),
-    defineField({
-      name: 'technologies',
-      title: 'Technologies',
-      of: [{ type: "reference", to: { type: "skill"} }],
-    }),
-    defineField({
-      name: 'points',
-      title: 'Points',
-      type: 'array',
+    },
+    {
+      name: "company",
+      title: "Company",
+      type: "text",
+    },
+    {
+      name: "dateStarted",
+      title: "DateStarted",
+      type: "date",
+    },
+    {
+      name: "dateEnded",
+      title: "DateEnded",
+      type: "date",
+    },
+    {
+      name: "isCurrentlyWorkingHere",
+      title: "IsCurrentlyWorkingHere",
+      type: "boolean",
+    },
+    {
+      name: "technologies",
+      title: "Technologies",
+      type: "array",
+      of: [{ type: "reference", to: { type: "skill" } }],
+    },
+    {
+      name: "points",
+      title: "Points",
+      type: "array",
       of: [{ type: "string" }],
-    }),
+    },
   ],
-})
+};
